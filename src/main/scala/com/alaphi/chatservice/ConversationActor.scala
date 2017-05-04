1de3e2c71ee051539e53f51ddf9879b5c49bb69d
alanp <alanphillips78@gmail.com>
2017-05-04 22:59:15 +0100

    Renamed TextMessage field as body

--- src/main/scala/com/alaphi/chatservice/ConversationActor.scala
+++ src/main/scala/com/alaphi/chatservice/ConversationActor.scala
@@ -2,8 +2,8 @@ package com.alaphi.chatservice
 
 import akka.actor.{Actor, ActorLogging, Props}
 
-case class TextMessage(conversationKey: String, textMessage: String)
-case class Event(conversationKey: String, textMessage: String)
+case class TextMessage(conversationKey: String, body: String)
+case class Event(conversationKey: String, body: String)
 
 class ConversationActor extends Actor with ActorLogging {
 
